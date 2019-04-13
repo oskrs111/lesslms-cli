3c1b4d9853e971941c321554569267cc3a73e5f8
Oscar Sanz Llopis <oskrs111@gmail.com>
2019-04-13 20:40:24 +0200

    First commit
    Help and Init functions implemented

---
+++ index.js
@@ -0,0 +1,50 @@
+/**
+ * @file lesslms-cli tool for serverless application deployment in AWS.
+ * @author Oscar Sanz <osanzl@uoc.edu>
+ * @module index
+ * @license MIT
+ */
+const minimist = require('minimist')
+const figlet = require('figlet');
+const clear = require('clear');
+/**
+ * Entry point for lesslms-cli.
+ * @function
+ */
+function _main() {
+    welcome();
+    const args = minimist(process.argv.slice(2));
+    let cmd = args._[0] || 'help';
+
+    if (args.version || args.v) {
+        cmd = 'version'
+    }
+
+    if (args.help || args.h) {
+        cmd = 'help'
+    }
+
+    switch (cmd) {
+        case 'init':
+        case 'deploy':
+        case 'help':
+        case 'version':
+            require(`./lib/${cmd}`)(args);
+            break;
+
+        default:
+            console.error(`"${cmd}" is not a valid command!`);
+            break;
+    }
+}
+
+/**
+ * Helper function for welcome message.
+ * @function
+ */
+function welcome() {
+    clear();
+    console.log(figlet.textSync('lesslms-cli'));
+}
+
+module.exports = _main;
